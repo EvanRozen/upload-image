af09bae32f30df9e53415cadbdf5942d7c31d25c
Evan ROZEN <evanrozen3@gmail.com>
2022-02-23 10:16:01 +0100

    v2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,39 +8,41 @@
     <link rel="stylesheet" href="./assets/styles.css">
     <link rel="stylesheet" href="./assets/uploadPreview.css">
 </head>
-
 <header>
-
     <h1>Module d'enregistrement d'images.</h1>
     <p>Mise en pratique PHP :Upload d'images</p>
-
-
-
 </header>
-
-
 <body>
-    
     <main>
         <div class="left">
             <label for="fileToUpload">veuillez choisir une image :</label>
-
-            <form action="" method="post" enctype="multipart/form-data">
-
+            <form action="" method="POST" enctype="multipart/form-data">
                 <input type="file" name="fileToUpload" id="fileToUpload"><br>
-
                 <button type="submit">Upload</button>
-
             </form>
         </div>
         <div class="right">
             <img src="" alt="image preview" id="imgPreview">
         </div>
     </main>
-
+    <?php
+        // var_dump($_FILES);
+        if(isset($_FILES['fileToUpload'])&& $_FILES['fileToUpload']['error'] == 0)
+        {
+            $tmp = $_FILES['fileToUpload']['tmp_name'];
+            $filename = $_FILES['fileToUpload']['name'];
+            $dest = 'img/';
+            if(move_uploaded_file($tmp,$dest.$filename))
+                echo 'téléchargement réussi';
+        }
+    ?>
 </body>
-
-
 <script src="./assets/script.js"></script>
 <script src="./assets/uploadPreview.js"></script>
 </html>
+
+
+
+
+
+
